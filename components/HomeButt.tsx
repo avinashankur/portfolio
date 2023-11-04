632c52cf6b27383860883610d3dd3ea32d546027
avinashankur <avinashhankur@gmail.com>
2023-11-04 20:29:53 +0530

    added:home butt

---
+++ components/HomeButt.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+const HomeButt = () => {
+  return (
+    <Link href="/" className="my-8 font-bold text-sky-400 text-lg hover:text-neutral-400 transition">&#60; Home</Link>
+  )
+}
+
+export default HomeButt
