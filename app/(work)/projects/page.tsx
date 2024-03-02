c38806cc2ef375eaac5ac6cc870a925d0963023a
avinashankur <avinashhankur@gmail.com>
2024-03-03 01:24:28 +0530

    added:projects page

--- app/(work)/projects/page.tsx
+++ app/(work)/projects/page.tsx
@@ -11,15 +11,66 @@ export default function Projects() {
         &#60; Home
       </Link>
 
-       <main className="px-4 xl:px-[5rem] pt-[10rem] min-h-screen">
+      <main className="px-4 xl:px-[5rem] pt-[10rem] min-h-screen">
         <h1 className="text-5xl lg:text-9xl tracking-wide font-anton uppercase text-neutral-400">
-          Projects
+          Project
         </h1>
-        <div className="uppercase text-5xl md:text-6xl flex flex-col my-24">
-          <Link href="https://harvey-bennet.vercel.app/" target="_blank" className="border-b-2 border-t-2 py-6 font-anton tracking-wider px-4">Harvey Bennet</Link>
-          <Link href="https://monoma-gamma.vercel.app/" target="_blank" className="border-b-2 py-6 font-anton tracking-wider px-4">Monoma</Link>
-          <Link href="https://nibble-phi.vercel.app/" target="_blank" className="border-b-2 py-6 font-anton tracking-wider px-4">NCS JSS</Link>
-          {/* <Link href="https://syndicate-school.vercel.app/" target="_blank" className="border-b-2 py-6 font-anton tracking-wider px-4">Syndicate</Link> */}
+
+        <div className="lg:my-40 mx-auto my-16 shadow flex flex-wrap md:grid grid-flow-row grid-cols-3 w-fit gap-4">
+          <Link
+            href=""
+            className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/monoma2.png')] bg-cover overflow-hidden group"
+          >
+            <div className="bg-neutral-950/70 lg:rotate-90 lg:translate-y-6 lg:-translate-x-[13.5rem] px-4 py-3 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:bg-green-500 group-hover:-rotate-0 transition duration-500 h-full">
+              <p className=" font-[impact] text-6xl">MONOMA</p>
+              <p className="py-4">Marketing Website</p>
+            </div>
+          </Link>
+          <Link
+            href=""
+            className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/ncs2.png')] bg-cover overflow-hidden group"
+          >
+            <div className="bg-neutral-950/70 h-full p-4 group-hover:bg-yellow-600 lg:translate-x-[13rem] lg:-translate-y-[1.5rem] transition duration-500 lg:-rotate-90 group-hover:rotate-0 group-hover:translate-x-0 group-hover:translate-y-0">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+                NCS JSS
+              </p>
+              <p className="uppercase text-sm">
+                With Next.js, Vercel, TailwindCSS
+              </p>
+            </div>
+          </Link>
+          <Link
+            href=""
+            className="bg-neutral-800 h-[20rem] md:h-[35rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 col-[2] row-[1/3] bg-[url('/harvey3.jpeg')] bg-cover flex flex-col justify-end overflow-hidden group"
+          >
+            <div className="bg-neutral-950/70 h-full p-4 lg:translate-y-[22rem] group-hover:bg-purple-600 group-hover:translate-y-0 transition duration-500">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+                Harvey Bennet Portfolio
+              </p>
+              <p className="uppercase text-sm">
+                With Next.js, Vercel, TailwindCSS
+              </p>
+            </div>
+          </Link>
+          <Link
+            href=""
+            className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/portfolio2.png')] bg-cover group overflow-hidden"
+          >
+            <div className="bg-pink-600 h-full p-4 group-hover:bg-neutral-950/70 group-hover:translate-y-[12rem] transition duration-500">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+                Portfolio Website
+              </p>
+              <p className="uppercase text-sm">
+                With Next.js, Vercel, TailwindCSS
+              </p>
+            </div>
+          </Link>
+          <Link
+            href=""
+            className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded border border-neutral-600 group relative flex items-center justify-center"
+          >
+            <p className="font-[impact] text-3xl animate-bounce">Coming Soon</p>
+          </Link>
         </div>
       </main>
     </>
