be1bf87b0b72bf3f19943ecb54ecf8c7cced044f
avinashankur <avinashhankur@gmail.com>
2024-03-03 11:02:33 +0530

    fixe:font settings

--- app/(work)/projects/page.tsx
+++ app/(work)/projects/page.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
             className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/monoma2.png')] bg-cover overflow-hidden group"
           >
             <div className="bg-neutral-950/70 lg:rotate-90 lg:translate-y-6 lg:-translate-x-[13.5rem] px-4 py-3 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:bg-green-500 group-hover:-rotate-0 transition duration-500 h-full">
-              <p className=" font-[impact] text-6xl">MONOMA</p>
+              <p className=" font-anton text-5xl lg:text-6xl">MONOMA</p>
               <p className="py-4">Marketing Website</p>
             </div>
           </Link>
@@ -31,7 +31,7 @@ export default function Projects() {
             className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/ncs2.png')] bg-cover overflow-hidden group"
           >
             <div className="bg-neutral-950/70 h-full p-4 group-hover:bg-yellow-600 lg:translate-x-[13rem] lg:-translate-y-[1.5rem] transition duration-500 lg:-rotate-90 group-hover:rotate-0 group-hover:translate-x-0 group-hover:translate-y-0">
-              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-anton tracking-4">
                 NCS JSS
               </p>
               <p className="uppercase text-sm">
@@ -44,7 +44,7 @@ export default function Projects() {
             className="bg-neutral-800 h-[20rem] md:h-[35rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 col-[2] row-[1/3] bg-[url('/harvey3.jpeg')] bg-cover flex flex-col justify-end overflow-hidden group"
           >
             <div className="bg-neutral-950/70 h-full p-4 lg:translate-y-[22rem] group-hover:bg-purple-600 group-hover:translate-y-0 transition duration-500">
-              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-anton tracking-4">
                 Harvey Bennet Portfolio
               </p>
               <p className="uppercase text-sm">
@@ -57,7 +57,7 @@ export default function Projects() {
             className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded  border border-neutral-600 bg-[url('/portfolio2.png')] bg-cover group overflow-hidden"
           >
             <div className="bg-pink-600 h-full p-4 group-hover:bg-neutral-950/70 group-hover:translate-y-[12rem] transition duration-500">
-              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-[impact] tracking-4">
+              <p className="font-medium uppercase mb-4 lg:text-6xl text-5xl font-anton tracking-4">
                 Portfolio Website
               </p>
               <p className="uppercase text-sm">
@@ -69,7 +69,7 @@ export default function Projects() {
             href=""
             className="bg-neutral-800 h-[17rem] w-[15rem] lg:w-[20rem] rounded border border-neutral-600 group relative flex items-center justify-center"
           >
-            <p className="font-[impact] text-3xl animate-bounce">Coming Soon</p>
+            <p className="font-anton text-3xl animate-bounce">Coming Soon</p>
           </Link>
         </div>
       </main>
